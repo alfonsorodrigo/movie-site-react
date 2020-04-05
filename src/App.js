26a85bf6f69731f9ba95063a63a3767a2a131bb8
Alfonso Feria <alfonso.feria@konfio.mx>
2020-04-05 15:28:13 -0500

    Add Routing Site

--- src/App.js
+++ src/App.js
@@ -1,10 +1,45 @@
 import React from "react";
+import { Layout } from "antd";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+// Importing Pages
+import Home from "./pages/Home";
+import NewMovies from "./pages/NewMovies";
+import Popular from "./pages/Popular";
+import Search from "./pages/Search";
+import Movie from "./pages/Movie";
+import Error404 from "./pages/Error404";
 
 const App = () => {
+  const { Header, Content } = Layout;
   return (
-    <div>
-      <h1>Estamos en APP</h1>
-    </div>
+    <Layout>
+      <Router>
+        <Header>Header</Header>
+        <Content>
+          <Switch>
+            <Route path="/" exact={true}>
+              <Home />
+            </Route>
+            <Route path="/new-movies" exact={true}>
+              <NewMovies />
+            </Route>
+            <Route path="/popular" exact={true}>
+              <Popular />
+            </Route>
+            <Route path="/search" exact={true}>
+              <Search />
+            </Route>
+            <Route path="/movie/:id" exact={true}>
+              <Movie />
+            </Route>
+            <Route path="*">
+              <Error404 />
+            </Route>
+          </Switch>
+        </Content>
+      </Router>
+    </Layout>
   );
 };
 
