3b1cbb539e691c769d73936193af643d258876d6
Alfonso Feria <alfonso.feria@konfio.mx>
2020-04-05 16:47:48 -0500

    Added useFetch Movies API

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,13 @@
 import React from "react";
+import useFetch from "../hooks/useFetch";
 
 const Home = () => {
+  const URL = "https://api.themoviedb.org/3";
+  const API_KEY = "763f06f50eb9842755c41502a2a11029";
+  const movies = useFetch(
+    `${URL}/movie/popular?api_key=${API_KEY}&language=en-US&page=1`
+  );
+  console.log(movies);
   return (
     <div>
       <h1>Estamos en Home</h1>
